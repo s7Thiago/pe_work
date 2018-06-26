74ea73c6b71547aec0dc74a5ef2b07fe82d4cad8
s7Thiago <thyagosousasilva@gmail.com>
2018-06-26 01:20:09 -0300

    Adicionando a codificação da interface e da estrutura básica do app. Primeira execução estável

--- app/src/main/java/br/com/thiagosousa/pesem_1/MainActivity.java
+++ app/src/main/java/br/com/thiagosousa/pesem_1/MainActivity.java
@@ -1,13 +1,81 @@
 package br.com.thiagosousa.pesem_1;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+import objetos.Ocorrencia;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private static final String SCREENTAG = "evento da MainActivity";
+
+    //views
+    private ListView listaPrincipal;
+    private TextInputEditText entradaPrincipal;
+    private ArrayList<Ocorrencia> ocorrenciasList;
+    private ArrayAdapter<Ocorrencia> adapter;
+    private Button adicionar;
+    private Button calcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews(true);
+
+        adicionar.setOnClickListener(this);
+        calcular.setOnClickListener(this);
+
+        ocorrenciasList = new ArrayList<Ocorrencia>();
+
+        adapter = new ArrayAdapter<Ocorrencia>(getBaseContext(), android.R.layout.simple_list_item_1, ocorrenciasList);
+        listaPrincipal.setAdapter(adapter);
+
+
+    }
+
+    public void initViews(boolean init) {
+        if (init) {
+            listaPrincipal = findViewById(R.id.listaPrincipal);
+            entradaPrincipal = findViewById(R.id.entradaDeTextoPrincipal);
+            adicionar = findViewById(R.id.botao_adicionar);
+            adicionar = findViewById(R.id.botao_calcular);
+        } else {
+            Log.w(SCREENTAG, "O recurso de carregamento dos componentes gráficos está desativado");
+        }
+    }
+
+    //Eventos de clique dos elementos visuais
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.botao_adicionar:
+                Log.w(SCREENTAG, "O botão adicionar de MainActivity foi pressionado");
+
+                String dado = entradaPrincipal.getText().toString();
+
+                //Por regra. a informação recebida do campo deve ter mais de 0 caractere
+                if(dado.length() > 0) {
+                    Ocorrencia ocorrencia = new Ocorrencia(Integer.valueOf(dado));
+
+                    ocorrenciasList.add(ocorrencia);
+                    entradaPrincipal.setText("");
+                    adapter.notifyDataSetChanged();
+
+                }else {
+                    entradaPrincipal.setError("Este campo não apresenta o resultado esperado\n Por favor, inssira um valor numérico");
+                }
+
+                break;
+            case R.id.botao_calcular:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/objetos/Ocorrencia.java
@@ -0,0 +1,32 @@
+package objetos;
+
+public class Ocorrencia {
+
+    private int valor;
+    private String descricao;
+
+    public Ocorrencia(int valor) {
+        setValor(valor);
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Descrição: " + descricao + "\nvalor: " + valor;
+    }
+}
