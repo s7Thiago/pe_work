ec310383a3cb55d3df1e0914958a53ee892a14d5
s7Thiago <thyagosousasilva@gmail.com>
2018-06-26 02:34:27 -0300

    Começando a adicionar a codificação dos cálculos estastísticos, e adicionando a dinâmica no código para identificar a alternância entre virtual e presencial

--- app/src/main/java/br/com/thiagosousa/pesem_1/MainActivity.java
+++ app/src/main/java/br/com/thiagosousa/pesem_1/MainActivity.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 
 import objetos.Ocorrencia;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String SCREENTAG = "evento da MainActivity";
 
     //views
@@ -23,6 +23,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ArrayAdapter<Ocorrencia> adapter;
     private Button adicionar;
     private Button calcular;
+
+    //variaveis para os calculos
+    int tam = 167, comprimento = 168, alternador = 1;
+    int virtual[] = new int[tam];
+    int presencial[] = new int[tam];
+    int diferenca[] = new int[tam];
+    double media, somatorio = 0, variancia, somaVar;
+    Ocorrencia ocorrencia;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +55,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             listaPrincipal = findViewById(R.id.listaPrincipal);
             entradaPrincipal = findViewById(R.id.entradaDeTextoPrincipal);
             adicionar = findViewById(R.id.botao_adicionar);
-            adicionar = findViewById(R.id.botao_calcular);
+            calcular = findViewById(R.id.botao_calcular);
         } else {
             Log.w(SCREENTAG, "O recurso de carregamento dos componentes gráficos está desativado");
         }
@@ -62,19 +71,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 String dado = entradaPrincipal.getText().toString();
 
                 //Por regra. a informação recebida do campo deve ter mais de 0 caractere
-                if(dado.length() > 0) {
-                    Ocorrencia ocorrencia = new Ocorrencia(Integer.valueOf(dado));
+                if (dado.length() > 0) {
+
+                    if (alternador == 0) {
+                        ocorrencia = new Ocorrencia(Integer.valueOf(dado));
+                        ocorrencia.setDescricao("Virtual");
+                        alternador = 1;
+                    } else {
+                        ocorrencia = new Ocorrencia(Integer.valueOf(dado));
+                        ocorrencia.setDescricao("Presencial");
+                        alternador = 0;
+                    }
 
                     ocorrenciasList.add(ocorrencia);
                     entradaPrincipal.setText("");
                     adapter.notifyDataSetChanged();
 
-                }else {
+                } else {
                     entradaPrincipal.setError("Este campo não apresenta o resultado esperado\n Por favor, inssira um valor numérico");
                 }
 
                 break;
             case R.id.botao_calcular:
+
+
                 break;
         }
     }
